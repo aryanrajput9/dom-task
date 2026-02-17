bceb5cd072e12a07272e115890c893e9c8f327d1
aryanrajput9 <babusahebaryan4@gmail.com>
2026-02-17 14:33:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,160 @@
+// let h2 = document.querySelector("h2");
+// let button = document.querySelector("button")
+
+// let change = false;
+
+// button.addEventListener("click", () => {
+//     if (change === false) {
+//         h2.innerText = "welcome";
+//         change = true
+//     } else {
+//         h2.innerText = "hellow";
+//         change = false
+//     }
+
+// })
+
+
+// let button = document.querySelector("button");
+// let box = document.querySelector(".box");
+// let colors = ["red", "green", "blue"];
+// let count = 0;
+
+// button.addEventListener("click", () => {
+
+//     box.style.backgroundColor = colors[count];
+
+//     count++;
+
+//     if (count === colors.length) {
+//         count = 0
+//     }
+// })
+
+
+
+// let button = document.querySelector("button");
+// let box = document.querySelector(".box");
+// let hide = false
+
+// button.addEventListener("click", () => {
+//     if (!hide) {
+//         box.style.display = "none";
+//         hide = true;
+//     } else {
+//         box.style.display = "flex";
+//         hide = false
+//     }
+// })
+
+
+
+// let right = document.querySelector(".right");
+// let left = document.querySelector(".left")
+// let box = document.querySelector(".box");
+
+// right.addEventListener("click", () => {
+//     box.style.transform = "rotate(100deg)"
+// })
+// left.addEventListener("click", () => {
+//     box.style.transform = "rotate(-100deg)"
+// })
+
+
+// let right = document.querySelector(".right");
+// let left = document.querySelector(".left")
+// let h2 = document.querySelector("h2");
+// let count = 0;
+
+
+// right.addEventListener("click", () => {
+//     count++;
+
+//     h2.innerText = count
+
+// })
+// left.addEventListener("click", () => {
+
+//     count--;
+//     h2.innerText = count
+// })
+
+
+
+// let right = document.querySelector(".right");
+// let left = document.querySelector(".left");
+
+// right.addEventListener("click", () => {
+//     document.body.style.backgroundColor = "black";
+//     document.body.style.color = "white"
+// })
+
+// left.addEventListener("click", () => {
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.color = "black"
+// })
+
+
+
+// let right = document.querySelector(".right");
+// let input = document.querySelector("#box");
+// let h2 = document.querySelector("h2");
+
+// right.addEventListener("click", () => {
+//     h2.innerText = input.value
+// })
+
+// let box = document.querySelector(".box");
+
+// box.addEventListener("mouseenter", () => {
+//     box.style.backgroundColor = "red";
+// });
+
+// box.addEventListener("mouseleave", () => {
+//     box.style.backgroundColor = "white";
+// });
+
+
+
+// let left = document.querySelector(".left");
+// let h2 = document.querySelector("h2");
+
+
+// left.addEventListener("click", () => {
+//     h2.innerText = "Button Clicked";
+// })
+
+
+// let button1 = document.querySelector("#btn1")
+
+// let button2 = document.querySelector("#btn2")
+
+// let button3 = document.querySelector("#btn3")
+// let circle = document.querySelectorAll(".box")
+
+// circle.forEach((elem, index) => {
+//     if (index === 0) {
+//         button1.addEventListener("click", () => {
+//             document.body.style.backgroundColor = "red"
+//         })
+//     }
+//     if (index === 1) {
+//         button2.addEventListener("click", () => {
+//             document.body.style.backgroundColor = "yellow"
+//         })
+//     }
+//     if (index === 2) {
+//         button3.addEventListener("click", () => {
+//             document.body.style.backgroundColor = "green"
+//         })
+//     }
+// })
+
+
+
+
+
+
+
+
+
